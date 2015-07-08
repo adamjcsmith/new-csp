3ab804032b333b55dc7800a5a975879849aab3c7
Adam Smith <adamjcsmith@gmail.com>
2015-07-08 17:10:44 -0400

    Added initial CSP theme

---
+++ wp-content/themes/csp-2015/footer.php
@@ -0,0 +1,5 @@
+<footer class="c p2">
+	<h6>&copy 2015 CSP Detailing</h6>
+</footer>
+</body>
+</html>
---
+++ wp-content/themes/csp-2015/header.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<head>
+	<title>CSP Detailing</title>
+	<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
+	<link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/custom.css"> 
+	<link href="//fonts.googleapis.com/css?family=Lato:300,400" rel="stylesheet" type="text/css">
+	<link href='//fonts.googleapis.com/css?family=Oswald:300,400' rel='stylesheet' type='text/css'>
+</head>
+<body>
+	<header>
+	
+		<div id="nav-icon"><i class="fa fa-bars"></i></div>
+	
+		<nav>
+			<li><a href="#">
+				<i class="fa fa-coffee"></i>
+				<span class="nav-text-dim">Me</span>
+			</a></li>
+		</nav>
+	</header>
